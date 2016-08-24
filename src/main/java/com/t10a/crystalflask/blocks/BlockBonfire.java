16e8a7d1a8484be4d0b50be47f0e091bc028865e
t10a <thomasccoote@gmail.com>
2016-08-24 19:57:15 +1000

    Added basic refilling functionality to the flask. Bonfire now emits light, and crackles faintly.

--- src/main/java/com/t10a/crystalflask/blocks/BlockBonfire.java
+++ src/main/java/com/t10a/crystalflask/blocks/BlockBonfire.java
@@ -6,13 +6,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
+import java.util.Random;
 
 public class BlockBonfire extends Block
 {
@@ -25,6 +31,7 @@ public class BlockBonfire extends Block
         setUnlocalizedName(Reference.MOD_ID + "." + Reference.BlockBase.BONFIRE.getUnlocalizedName());
         setRegistryName(Reference.BlockBase.BONFIRE.getRegistryName());
         setCreativeTab(CrystalFlask.ESTUSTAB);
+        this.setLightLevel(5.0F);
     }
 
     @SuppressWarnings("deprecation")
@@ -50,8 +57,27 @@ public class BlockBonfire extends Block
         return BOUNDING_BOX;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn) {
         super.addCollisionBoxToList(pos, entityBox, collidingBoxes, BOUNDING_BOX);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SuppressWarnings("incomplete-switch")
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
+    {
+        double d0 = (double)pos.getX() + 0.5D;
+        double d1 = (double)pos.getY() + rand.nextDouble() * 6.0D / 16.0D;
+        double d2 = (double)pos.getZ() + 0.5D;
+        double d3 = 0.52D;
+        double d4 = rand.nextDouble() * 0.6D - 0.3D;
+
+        if (rand.nextDouble() < 0.3D)
+        {
+            worldIn.playSound((double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+        }
+        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1 + d4, d2, 0.0D, 0.0D, 0.0D, new int[0]);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1 + d4, d2, 0.0D, 0.0D, 0.0D, new int[0]);
     }
 }
--- src/main/java/com/t10a/crystalflask/items/ItemCrystalFlask.java
+++ src/main/java/com/t10a/crystalflask/items/ItemCrystalFlask.java
@@ -2,20 +2,17 @@ package com.t10a.crystalflask.items;
 
 import com.t10a.crystalflask.CrystalFlask;
 import com.t10a.crystalflask.Reference;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import com.t10a.crystalflask.init.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.IItemPropertyGetter;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -23,48 +20,124 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import javax.annotation.Nullable;
 import java.util.List;
 
+//Yes, this is a ton of code. I'll try to clean it up ASAP.
 public class ItemCrystalFlask extends Item
 {
     public ItemCrystalFlask()
     {
         setUnlocalizedName(Reference.MOD_ID + "." + Reference.ItemBase.ESTUS.getUnlocalizedName());
         setRegistryName(Reference.ItemBase.ESTUS.getRegistryName());
+        this.addPropertyOverride(new ResourceLocation("empty"), new IItemPropertyGetter()
+        {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+            {
+                return ItemCrystalFlask.getUsable(stack) ? 0.0F : 1.0F;
+            }
+        });
         setCreativeTab(CrystalFlask.ESTUSTAB);
         this.setMaxStackSize(1);
     }
-
-    /*
-    @Override
-    public void addInformation(ItemStack stack, EntityPlayer player, List<String> list, boolean par4)
+    //Catches whether or not this item is drinkable or not.
+    public static boolean getUsable(ItemStack stack)
     {
-        list.add("A crystallised flask, containing a magical flame. Drink to restore HP.");
-        list.add("(totally not an estus flask)");
+        if(EnumAction.DRINK == stack.getItemUseAction())
+        {
+            return true;
+        }
+        else return false;
     }
-    */
+    //If it's drinkable, return the drinking time ticks. If not, return 1 tick for the empty/null flask.
     public int getMaxItemUseDuration(ItemStack stack)
     {
-        return 32;
+        if(getUsable(stack))
+        {
+            return 32;
+        }
+        else return 1;
     }
 
+    //Check whether the item actually has data, and set the EnumAction based on the result.
     public EnumAction getItemUseAction(ItemStack stack)
     {
-        return EnumAction.DRINK;
+        NBTTagCompound nbt;
+        if (stack.hasTagCompound())
+        {
+            nbt = stack.getTagCompound();
+        }
+        else
+        {
+            nbt = new NBTTagCompound();
+        }
+        if(nbt.hasKey("Uses"))
+        {
+            if(nbt.getInteger("Uses") <= 0)
+            {
+                return EnumAction.NONE;
+            }
+            else return EnumAction.DRINK;
+        }
+        else
+        {
+            return EnumAction.NONE;
+        }
     }
 
+    //Just checking to see if the item can be right-clicked.
     public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand)
     {
         playerIn.setActiveHand(hand);
         return new ActionResult(EnumActionResult.SUCCESS, stack);
     }
 
+    /*Big chunk o' code. This checks whether if the player can edit blocks (this'll be useful for when this can be used to upgrade the flask.), and then check the Item for NBT,
+    * and then finally, if the block it's checking is a Bonfire it'll refill it to the Max Uses NBT (This also happens when Uses is applied to the item.)
+    */
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if (!playerIn.canPlayerEdit(pos.offset(facing), facing, stack))
+        {
+            return EnumActionResult.FAIL;
+        }
+        else
+        {
+            NBTTagCompound nbt;
+            IBlockState iblockstate = world.getBlockState(pos);
+            Block block = iblockstate.getBlock();
+
+            if (stack.hasTagCompound())
+            {
+                nbt = stack.getTagCompound();
+            }
+            else
+            {
+                nbt = new NBTTagCompound();
+            }
+
+            if (nbt.hasKey("Uses"))
+            {
+                if (facing != EnumFacing.DOWN && block == ModBlocks.bonfire)
+                {
+                    nbt.setInteger("Uses", nbt.getInteger("Max Uses"));
+                    return EnumActionResult.SUCCESS;
+                }
+                else return EnumActionResult.PASS;
+            }
+            else
+            {
+                nbt.setInteger("Uses", 1);
+                nbt.setInteger("Max Uses", 1);
+            }
+            stack.setTagCompound(nbt);
+
+            return EnumActionResult.PASS;
+        }
+    }
+
+    //Another chunk. If this item has metadata, it'll get it. Then, if it has the metadata "Uses", it'll check to see if it is more than 0. If it is, and this isn't on the server side, it'll
     @Nullable
     public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving)
     {
-        if (!worldIn.isRemote)
-        {
-            entityLiving.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH));
-        }
-
         NBTTagCompound nbt;
         if (stack.hasTagCompound())
         {
@@ -77,11 +150,19 @@ public class ItemCrystalFlask extends Item
 
         if (nbt.hasKey("Uses"))
         {
-            nbt.setInteger("Uses", nbt.getInteger("Uses") + 1);
+            if(nbt.getInteger("Uses") > 0)
+            {
+                nbt.setInteger("Uses", nbt.getInteger("Uses") - 1);
+                if (!worldIn.isRemote)
+                {
+                    entityLiving.addPotionEffect(new PotionEffect(MobEffects.INSTANT_HEALTH, 1, 1));
+                }
+            }
         }
         else
         {
             nbt.setInteger("Uses", 1);
+            nbt.setInteger("Max Uses", 1);
         }
         stack.setTagCompound(nbt);
 
@@ -92,6 +173,7 @@ public class ItemCrystalFlask extends Item
     @Override
     public void addInformation(ItemStack stack, EntityPlayer player, List lores, boolean b)
     {
+        lores.add("A lone flask, used to contain §mestus§r§7 a magic flame that restores the health of the drinker. ");
         if (stack.hasTagCompound() && stack.getTagCompound().hasKey("Uses"))
         {
             lores.add("Uses: " + Integer.toString(stack.getTagCompound().getInteger("Uses")));
